fa1e965dd3d8efa786916a730a4f1331a1e0f011
wantg <wantg78@gmail.com>
2025-05-15 22:10:19 +0800

    init

---
+++ addons/combined-script-editor/plugin.gd
@@ -0,0 +1,71 @@
+@tool
+extends EditorPlugin
+
+var editor_main_screen: VBoxContainer
+var resize_panel: TextureRect
+var script_editor_size: Vector2 = Vector2.ZERO
+
+func _enter_tree() -> void:
+	editor_main_screen = EditorInterface.get_editor_main_screen()
+	editor_main_screen.sort_children.connect(combine_script_editor)
+	editor_main_screen.resized.connect(combine_script_editor)
+
+	resize_panel = TextureRect.new()
+	resize_panel.stretch_mode = TextureRect.StretchMode.STRETCH_KEEP_CENTERED
+	resize_panel.texture = EditorInterface.get_base_control().get_theme_icon("Hsize", "EditorIcons")
+	resize_panel.custom_minimum_size = Vector2(20, 20)
+	resize_panel.gui_input.connect(on_resize_panel_gui_input)
+
+	var script_editor = EditorInterface.get_script_editor()
+	script_editor.get_child(0).get_child(0).add_child(resize_panel)
+	script_editor.get_child(0).get_child(0).move_child(resize_panel, 0)
+
+	script_editor_size = editor_main_screen.size / 2
+
+func combine_script_editor():
+	var _2d_editor_view: VBoxContainer = editor_main_screen.get_child(0)
+	var _3d_editor_view: VBoxContainer = editor_main_screen.get_child(1)
+	var script_editor_view: MarginContainer = editor_main_screen.get_child(2)
+	var game_view: MarginContainer = editor_main_screen.get_child(3)
+	var asset_lib_view: PanelContainer = editor_main_screen.get_child(4)
+	var scene_editor: VBoxContainer
+
+	if _2d_editor_view.visible:
+		scene_editor = _2d_editor_view
+	elif _3d_editor_view.visible:
+		scene_editor = _3d_editor_view
+	else:
+		resize_panel.visible = false
+		if game_view.visible || asset_lib_view.visible:
+			await get_tree().create_timer(0.001).timeout
+			script_editor_view.visible = false
+		return
+
+	var editor_size = editor_main_screen.size
+
+	var script_editor_width = clamp(
+		script_editor_size.x,
+		script_editor_view.get_minimum_size().x,
+		editor_size.x - scene_editor.get_minimum_size().x,
+	)
+
+	scene_editor.size = Vector2(editor_size.x - script_editor_width, editor_size.y)
+	script_editor_view.position = Vector2(scene_editor.size.x, scene_editor.position.y)
+	script_editor_view.size = Vector2(editor_size.x - scene_editor.size.x, scene_editor.size.y)
+	script_editor_view.visible = true
+	resize_panel.visible = true
+
+func on_resize_panel_gui_input(event: InputEvent):
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			resize_panel.set_meta("dragging", true)
+			resize_panel.set_meta("drag_start_position", event.global_position)
+		elif event.is_released():
+			resize_panel.set_meta("dragging", false)
+			var script_editor_view: MarginContainer = editor_main_screen.get_child(2)
+			script_editor_size = script_editor_view.size
+	elif event is InputEventMouseMotion:
+		if resize_panel.get_meta("dragging", false):
+			var drag_start_position = resize_panel.get_meta("drag_start_position")
+			script_editor_size -= event.relative
+			combine_script_editor()
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Godot Combined Script Editor
+
+<img src="https://raw.githubusercontent.com/wantg/godot-combined-script-editor/master/addons/combined-script-editor/preview/preview-1.png">
