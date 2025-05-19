1675e13f7d0b42b758282676f3074dcea733bc3c
wantg <wantg78@gmail.com>
2025-05-19 12:16:21 +0800

    add Expand Modified only Button

--- addons/combined-script-editor/plugin.gd
+++ addons/combined-script-editor/plugin.gd
@@ -2,23 +2,26 @@
 extends EditorPlugin
 
 const config_path := ".godot/plugin_config.ini"
-var editor_main_screen: VBoxContainer
+var base_control: Control = EditorInterface.get_base_control()
+var inspector: EditorInspector = EditorInterface.get_inspector()
+var editor_main_screen: VBoxContainer = EditorInterface.get_editor_main_screen()
+var script_editor: ScriptEditor = EditorInterface.get_script_editor()
 var resize_panel: TextureRect
 var script_editor_size: Vector2 = Vector2.ZERO
 var config: Dictionary
 
 func _enter_tree() -> void:
-	editor_main_screen = EditorInterface.get_editor_main_screen()
+	improve_inspector()
+	
 	editor_main_screen.sort_children.connect(combine_script_editor)
 	editor_main_screen.resized.connect(combine_script_editor)
 
 	resize_panel = TextureRect.new()
 	resize_panel.stretch_mode = TextureRect.StretchMode.STRETCH_KEEP_CENTERED
-	resize_panel.texture = EditorInterface.get_base_control().get_theme_icon("Hsize", "EditorIcons")
+	resize_panel.texture = base_control.get_theme_icon("Hsize", "EditorIcons")
 	resize_panel.custom_minimum_size = Vector2(20, 20)
 	resize_panel.gui_input.connect(on_resize_panel_gui_input)
 
-	var script_editor = EditorInterface.get_script_editor()
 	script_editor.get_child(0).get_child(0).add_child(resize_panel)
 	script_editor.get_child(0).get_child(0).move_child(resize_panel, 0)
 
@@ -28,6 +31,40 @@ func _enter_tree() -> void:
 func _exit_tree() -> void:
 	config["script_editor_size"] = script_editor_size
 	save_config()
+
+func improve_inspector():
+	inspector.edited_object_changed.connect(on_inspector_edited_object_changed)
+
+	# add Expand Modified only Button
+	var property_filter_bar: HBoxContainer = get_inspector_property_filter_bar()
+	var property_filter_button = property_filter_bar.get_children().back() as MenuButton
+	var expand_modified_only_button := Button.new()
+	expand_modified_only_button.name = "expand_modified_only_button"
+	expand_modified_only_button.flat = true
+	expand_modified_only_button.icon = base_control.get_theme_icon("EditInternal", "EditorIcons")
+	expand_modified_only_button.focus_mode = Control.FocusMode.FOCUS_NONE
+	expand_modified_only_button.tooltip_text = "Expand Non-Default only"
+	expand_modified_only_button.disabled = property_filter_button.disabled
+	expand_modified_only_button.pressed.connect(func():
+		var popup_menu: PopupMenu = property_filter_button.get_popup()
+		popup_menu.id_pressed.emit(popup_menu.get_item_id(1))
+		popup_menu.id_pressed.emit(popup_menu.get_item_id(2))
+	)
+	property_filter_button.add_sibling(expand_modified_only_button)
+
+func on_inspector_edited_object_changed():
+	# Expand Modified only Button disable or not
+	var property_filter_bar: HBoxContainer = get_inspector_property_filter_bar()
+	var property_filter_button = property_filter_bar.get_children()[-2]
+	var expand_modified_only_button = property_filter_bar.get_children().back()
+	await get_tree().create_timer(0.001).timeout
+	expand_modified_only_button.disabled = property_filter_button.disabled
+
+func get_inspector_property_filter_bar() -> HBoxContainer:
+	for control in inspector.get_parent().get_children() as Array[Control]:
+		if control.get_child_count() > 1 && control.get_child(1) is MenuButton && control.get_child(1).icon == base_control.get_theme_icon("Tools", "EditorIcons"):
+			return control
+	return null
 
 func combine_script_editor():
 	var _2d_editor_view: VBoxContainer = editor_main_screen.get_child(0)
